575ee4fa16efff94d7e24a059a0c2e2f8a5074c9
Danilo Barros <danilobjr@gmail.com>
2020-08-07 18:42:15 -0300

    feat(components): add TeacherItem

---
+++ src/common/teacher-item/TeacherItem.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from 'react'
+import { View, Image, Text } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+
+import heartIcon from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+
+import { styles } from './styles'
+
+const TeacherItem: FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://github.com/danilobjr.png' }}
+        />
+
+        <View style={styles.info}>
+          <Text style={styles.name}>Danilo Barros</Text>
+          <Text style={styles.subject}>Math</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+        {/* {'\n'}{'\n'}
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. */}
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Price/Hour {'   '}
+          <Text style={styles.value}>US$ 20.00</Text>
+        </Text>
+
+        <View style={styles.actions}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Contact</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export { TeacherItem }
---
+++ src/common/teacher-item/styles.ts
@@ -0,0 +1,93 @@
+import { StyleSheet } from 'react-native'
+import { colors, fonts } from '../../styles'
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+    backgroundColor: colors.boxBase,
+    borderWidth: 1,
+    borderColor: colors.lineOnWhite,
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  profile: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 24,
+  },
+  avatar: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#eee',
+  },
+  info: {
+    marginLeft: 16,
+  },
+  name: {
+    fontFamily: fonts.archivoBold,
+    color: colors.textTitle,
+    fontSize: 20,
+  },
+  subject: {
+    fontFamily: fonts.poppins,
+    color: colors.textBase,
+    fontSize: 12,
+    marginTop: 4,
+  },
+  bio: {
+    marginHorizontal: 24,
+    fontFamily: fonts.poppins,
+    fontSize: 14,
+    lineHeight: 24,
+    color: colors.textBase,
+  },
+  footer: {
+    padding: 24,
+    marginTop: 24,
+    backgroundColor: colors.boxFooter,
+    alignItems: 'center',
+  },
+  price: {
+    fontFamily: fonts.poppins,
+    color: colors.textBase,
+    fontSize: 14,
+  },
+  value: {
+    fontFamily: fonts.archivoBold,
+    color: colors.primary,
+    fontSize: 16,
+  },
+  actions: {
+    flexDirection: 'row',
+    marginTop: 16,
+  },
+  favoriteButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: colors.primary,
+    borderRadius: 8,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contactButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: colors.secundary,
+    borderRadius: 8,
+    marginRight: 8,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contactButtonText: {
+    fontFamily: fonts.archivoBold,
+    fontSize: 16,
+    color: colors.buttonText,
+    marginLeft: 16,
+  },
+})
+
+export { styles }
